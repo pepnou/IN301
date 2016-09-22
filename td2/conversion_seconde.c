d6bcdc3b52ac94b9e509fda3fcae8d7f16c8bf79
pepnou <pepnou1@gmail.com>
2016-09-22 12:51:18 +0200

    ajout multiplication_egyptienne

--- td2/conversion_seconde.c
+++ td2/conversion_seconde.c
@@ -6,18 +6,21 @@
 int main()
 {
 	int temps,heures,minutes,secondes;
-	temps = 52442;
+	
+	srand(time(NULL));
+	temps = rand();
+	
 	secondes = temps%60;
 	heures = temps /3600;
 	minutes = temps/60 - heures*60;
 	
-	printf("%d correspond a : ",temps);
+	printf("%d secondes correspond a : ",temps);
 	if(heures>1) printf("%d heures, ",heures);
-	else ("%d heure, \n",heures);
+	else printf("%d heure, \n",heures);
 	if(minutes>1) printf("%d minutes, ",minutes);
-	else ("%d minutes, \n",minutes);
+	else printf("%d minutes, \n",minutes);
 	if(secondes>1) printf("et %d secondes\n",secondes);
-	else ("et %d seconde\n",secondes);
+	else printf("et %d seconde\n",secondes);
 	
 	
 	return 0;
---
+++ td2/multiplication_egyptienne.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main()
+{
+	int X,Y,reste;
+	reste = 0;
+	
+	srand(time(NULL));
+	//X = rand();
+	//Y = rand();
+	X=23;
+	Y=87;
+	
+	printf("%d x %d\n",X,Y);
+	
+	while(X>1)
+	{
+		if(X%2 == 0)
+		{
+			X = X/2;
+			Y = Y*2;
+		}
+		else
+		{
+			X--;
+			reste = reste+Y;
+		}
+		printf("= %d x %d + %d\n",X,Y,reste);
+	}
+	printf("= %d\n",Y+reste);
+	
+	return 0;
+}
+
