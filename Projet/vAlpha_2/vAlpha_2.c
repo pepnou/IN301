f53ceb186580bd737b3f790909decd29c3df912e
pepnou <pepnou1@gmail.com>
2016-11-11 21:09:59 +0100

    oui

--- Projet/vAlpha_2/vAlpha_2.c
+++ Projet/vAlpha_2/vAlpha_2.c
@@ -13,6 +13,9 @@
 #define largeur 400
 #define hauteur 400
 
+static Uint8 *audio_pos; // global pointer to the audio buffer to be played
+static Uint32 audio_len; // remaining length of the sample we have to play
+
 struct pos
 {
 	int x;
@@ -33,17 +36,20 @@ typedef struct level LEVEL;
 void affichelvl(SDL_Surface *ecran,LEVEL niveau);
 LEVEL lecture_fichier();
 void pause();
+void jouer_musique();
+void my_audio_callback(void *userdata, Uint8 *stream, int len);
 
-
-void affichelvl(SDL_Surface *ecran,LEVEL niveau);
 main()
 {
 	SDL_Surface *ecran = NULL;
 
 	TTF_Init();
     SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_AUDIO);
+    
+    
 
-    SDL_WM_SetCaption("Test",NULL);
+    SDL_WM_SetCaption("Sokoban vAlpha2",NULL);
 
     ecran = SDL_SetVideoMode(largeur,hauteur,32,SDL_HWSURFACE);
 	
@@ -52,6 +58,8 @@ main()
 	lvl=lecture_fichier();
 	
 	affichelvl(ecran,lvl);
+	
+	jouer_musique();
 	
 	pause();
 	
@@ -187,16 +195,112 @@ void pause()
 }
 
 
+void jouer_musique()
+{
+	// local variables
+	static Uint32 wav_length; // length of our sample
+	static Uint8 *wav_buffer; // buffer containing our audio file
+	static SDL_AudioSpec wav_spec; // the specs of our piece of music
+	
+	
+	/* Load the WAV */
+	// the specs, length and buffer of our wav are filled
+	SDL_LoadWAV("01 Hammerwatch.wav", &wav_spec, &wav_buffer, &wav_length);
 
+	
+	
+	// set the callback function
+	wav_spec.callback = my_audio_callback;
+	wav_spec.userdata = NULL;
+	// set our global static variables
+	audio_pos = wav_buffer; // copy sound buffer
+	audio_len = wav_length; // copy file length
+	
+	/* Open the audio device */
+	if ( SDL_OpenAudio(&wav_spec, NULL) < 0 ){
+	  fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError());
+	  exit(-1);
+	}
+	
+	/* Start playing */
+	SDL_PauseAudio(0);
 
+	// wait until we're don't playing
+	while ( audio_len > 0 ) {
+		SDL_Delay(100); 
+	}
+	
+	// shut everything down
+	SDL_CloseAudio();
+	SDL_FreeWAV(wav_buffer);
+	
+}
 
+void my_audio_callback(void *userdata, Uint8 *stream, int len) {
+	
+	if (audio_len ==0)
+		return;
+	
+	len = ( len > audio_len ? audio_len : len );
+	//SDL_memcpy (stream, audio_pos, len); 					// simply copy from one buffer into the other
+	SDL_MixAudio(stream, audio_pos, len, SDL_MIX_MAXVOLUME);// mix from one buffer into another
+	
+	audio_pos += len;
+	audio_len -= len;
+}
 
+/*
+#define MUS_PATH "Roland-GR-1-Trumpet-C5.wav"
 
+// prototype for our audio callback
+// see the implementation for more information
+void my_audio_callback(void *userdata, Uint8 *stream, int len);
 
+int main(int argc, char* argv[]){
+	
+	// local variables
+	static Uint32 wav_length; // length of our sample
+	static Uint8 *wav_buffer; // buffer containing our audio file
+	static SDL_AudioSpec wav_spec; // the specs of our piece of music
+	
+	
+	// Load the WAV 
+	// the specs, length and buffer of our wav are filled
+	if( SDL_LoadWAV(MUS_PATH, &wav_spec, &wav_buffer, &wav_length) == NULL ){
+	  return 1;
+	}
+	// set the callback function
+	wav_spec.callback = my_audio_callback;
+	wav_spec.userdata = NULL;
+	// set our global static variables
+	audio_pos = wav_buffer; // copy sound buffer
+	audio_len = wav_length; // copy file length
+	
+	// Open the audio device 
+	if ( SDL_OpenAudio(&wav_spec, NULL) < 0 ){
+	  fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError());
+	  exit(-1);
+	}
+	
+	// Start playing 
+	SDL_PauseAudio(0);
 
+	// wait until we're don't playing
+	while ( audio_len > 0 ) {
+		SDL_Delay(100); 
+	}
+	
+	// shut everything down
+	SDL_CloseAudio();
+	SDL_FreeWAV(wav_buffer);
 
+}
 
-
+// audio callback function
+// here you have to copy the data of your audio buffer into the
+// requesting audio buffer (stream)
+// you should only copy as much as the requested length (len)
+*/
 
 
 
