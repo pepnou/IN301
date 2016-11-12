0a5e96f32c3167188ddcae05227db1324cfdf281
pepnou <pepnou1@gmail.com>
2016-11-12 13:51:32 +0100

    oui

--- Projet/vBeta_1/vBeta_1.c
+++ Projet/vBeta_1/vBeta_1.c
@@ -8,7 +8,8 @@
 #include "affichage.h"
 #include "lecture_ecriture.h"
 
-
+void creation_lvl(SDL_Surface *ecran);
+LEVEL init_lvl(LEVEL niveau);
 
 int main()
 {
@@ -18,19 +19,90 @@ int main()
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Init(SDL_INIT_AUDIO);
     
-    
-
     SDL_WM_SetCaption("Sokoban vAlpha2",NULL);
 
     ecran = SDL_SetVideoMode(largeur_fenetre,hauteur_fenetre,32,SDL_HWSURFACE);
 	
 	LEVEL lvl;
 	
-	lvl = lecture_fichier(2);
+	//lvl = lecture_fichier(2);
+	//affichelvl(ecran,lvl);
 	
-	affichelvl(ecran,lvl);
+	creation_lvl(ecran);
 	
-	pause();
+	//pause();
 	
 	exit(EXIT_SUCCESS);
 }
+
+void creation_lvl(SDL_Surface *ecran)
+{
+	//printf("1\n");
+	
+	LEVEL niveau;
+	niveau.width=10;
+	niveau.height=10;
+	
+	//niveau = init_lvl(niveau);
+	
+	
+	int i,j;
+	
+	for(j=0;j<niveau.height;j++)
+	{
+		for(i=0;i<niveau.width;i++)
+		{
+			niveau.T[i][j] = 0;
+		}
+	}
+	
+	printf("2\n");
+	
+	affichelvl(ecran,niveau);
+	
+	printf("3\n");
+	
+	SDL_Event event;
+	int continuer = 1;
+	
+	
+	
+	while(continuer)
+	{
+		
+		SDL_WaitEvent(&event);
+		switch(event.type)
+		{
+			case SDL_KEYDOWN:
+				if(event.key.keysym.sym == SDLK_RETURN) continuer = 0;
+				break;
+			case SDL_MOUSEBUTTONUP:
+				niveau.T[event.button.x/(largeur_fenetre/niveau.width)][event.button.y/(hauteur_fenetre/niveau.height)] = (niveau.T[event.button.x/(largeur_fenetre/niveau.width)][event.button.y/(hauteur_fenetre/niveau.height)] + 1 ) % 7;
+				affichelvl(ecran,niveau);
+				break;
+			default:
+				break;
+		}
+	}
+	
+	
+	
+}
+
+LEVEL init_lvl(LEVEL niveau)
+{
+	int i,j;
+	
+	for(j=0;j<niveau.height;j++)
+	{
+		for(i=0;i<niveau.width;i++)
+		{
+			niveau.T[i][j] = 0;
+		}
+	}
+	
+	return niveau;
+}
+
+
+
