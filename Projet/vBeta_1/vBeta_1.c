2107b71d7adc916cb79bc5ee29395eb2893409d3
pepnou <pepnou1@gmail.com>
2016-12-31 11:43:51 +0100

    oui

--- Projet/vBeta_1/vBeta_1.c
+++ Projet/vBeta_1/vBeta_1.c
@@ -677,66 +677,113 @@ Play_Event attendre_evenement_invert(LEVEL niveau)
 	
 	SDL_EnableKeyRepeat(10, 10);
 	
-	SDL_Event action;
+	SDL_Event event;
 	int continuer=1;
-	
+	int position_bandeau = niveau.width*16 + 20 - 150;
+	int x,y;
 	
 	while(continuer)
 	{
-		SDL_WaitEvent(&action);
-		if(action.type==SDL_KEYDOWN)
+		SDL_WaitEvent(&event);
+		switch(event.type)
 		{
-			switch(action.key.keysym.sym)
-			{
-				case SDLK_UP:
-					continuer = 0;
-					evenement.deplacement.y=-1;
-					evenement.deplacement.x=0;
-					break;
-				case SDLK_DOWN:
-					continuer = 0;
-					evenement.deplacement.y=1;
-					evenement.deplacement.x=0;
-					break;
-				case SDLK_RIGHT:
-					continuer = 0;
-					evenement.deplacement.x=1;
-					evenement.deplacement.y=0;
-					break;
-				case SDLK_LEFT:
-					continuer = 0;
-					evenement.deplacement.x=-1;
-					evenement.deplacement.y=0;
-					break;
-				case SDLK_u:
+			case SDL_KEYDOWN:
+				switch(event.key.keysym.sym)
+				{
+					case SDLK_UP:
+						continuer = 0;
+						evenement.deplacement.y=-1;
+						evenement.deplacement.x=0;
+						break;
+					case SDLK_DOWN:
+						continuer = 0;
+						evenement.deplacement.y=1;
+						evenement.deplacement.x=0;
+						break;
+					case SDLK_RIGHT:
+						continuer = 0;
+						evenement.deplacement.x=1;
+						evenement.deplacement.y=0;
+						break;
+					case SDLK_LEFT:
+						continuer = 0;
+						evenement.deplacement.x=-1;
+						evenement.deplacement.y=0;
+						break;
+					case SDLK_u:
+						continuer = 0;
+						evenement.event = E_UNDO;
+						break;
+					case SDLK_r:
+						continuer = 0;
+						evenement.event = E_REDO;
+						break;
+					case SDLK_i:
+						continuer = 0;
+						evenement.event = E_INIT;
+						break;
+					/*case SDLK_p:
+						continuer = 0;
+						evenement.event = E_PREVIOUS;
+						break;
+					case SDLK_s:
+						continuer = 0;
+						evenement.event = E_NEXT;
+						break;*/
+					case SDLK_q: case SDLK_ESCAPE:
+						continuer = 0;
+						evenement.event = E_QUIT;
+						break;
+					case SDLK_RETURN:
+						continuer = 0;
+						evenement.event = E_CONFIRM;
+						break;
+				}
+				break;
+			case SDL_MOUSEBUTTONUP:
+				if(position_clic_encadre(position_bandeau + 10,position_bandeau + 49,20,80,event))
+				{
 					continuer = 0;
 					evenement.event = E_UNDO;
-					break;
-				case SDLK_r:
+				}
+				if(position_clic_encadre(position_bandeau + 58,position_bandeau + 97,20,80,event))
+				{
 					continuer = 0;
 					evenement.event = E_REDO;
-					break;
-				case SDLK_i:
-					continuer = 0;
-					evenement.event = E_INIT;
-					break;
-				case SDLK_p:
+				}
+				/*if(position_clic_encadre(position_bandeau + 106,position_bandeau + 145,20,80,event))
+				{
 					continuer = 0;
 					evenement.event = E_PREVIOUS;
-					break;
-				case SDLK_s:
+				}*/
+				if(position_clic_encadre(position_bandeau + 154,position_bandeau + 193,20,80,event))
+				{
 					continuer = 0;
-					evenement.event = E_NEXT;
-					break;
-				case SDLK_q: case SDLK_ESCAPE:
-					continuer = 0;
-					evenement.event = E_QUIT;
-					break;
-				case SDLK_RETURN:
+					evenement.event = E_INIT;
+				}
+				if(position_clic_encadre(position_bandeau + 202,position_bandeau + 241,20,80,event))
+				{
 					continuer = 0;
 					evenement.event = E_CONFIRM;
-					break;
-			}
+				}
+				if(position_clic_encadre(position_bandeau + 250,position_bandeau + 289,20,80,event))
+				{
+					continuer = 0;
+					evenement.event = E_QUIT;
+				}
+				if(position_clic_encadre(20,niveau.width*32 + 20,100,niveau.height*32 + 100,event))
+				{
+					x = (event.button.x-20)/32;
+					y = (event.button.y-100)/32;
+					
+					if(((x-niveau.joueur.x) + (y-niveau.joueur.y) == -1)||((x-niveau.joueur.x) + (y-niveau.joueur.y) == 1))
+					{	
+						evenement.deplacement.x= x - niveau.joueur.x;
+						evenement.deplacement.y= y - niveau.joueur.y;
+						continuer = 0;
+					}
+				}
+				break;
 		}
 	}
 	
